165c2256581f14f4aff9ff9c1c4e700929cd4a32
Yu Cao <acw759@vscode-01.apocrita>
2025-03-05 08:41:00 +0000

    set avante.nvim version to false

--- lua/plugins/avante.lua
+++ lua/plugins/avante.lua
@@ -1,7 +1,7 @@
 return {
   "yetone/avante.nvim",
   cmd = { "AvanteAsk" },
-  version = "*", -- set this if you want to always pull the latest change
+  version = false, -- Set this to "*" to always pull the latest release version, or set it to false to update to the latest code changes.
   -- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
   opts = {
     provider = "claude",
