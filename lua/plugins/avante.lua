d515e48faab9cde749d52aa86c195e0336b842fa
max <yu.carl@outlook.com>
2025-03-09 20:33:06 +0000

    revert claude version to 3.5 and add more default config

--- lua/plugins/avante.lua
+++ lua/plugins/avante.lua
@@ -5,11 +5,75 @@ return {
   -- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
   opts = {
     provider = "claude",
+    ---@type AvanteSupportedProvider
     claude = {
       endpoint = "https://api.anthropic.com",
-      model = "claude-3-7-sonnet-20250219",
+      model = "claude-3-5-sonnet-20241022",
+      timeout = 30000, -- Timeout in milliseconds
+      temperature = 0,
+      max_tokens = 8000,
+      disable_tools = true, -- disable tools!
+    },
+    ---@type AvanteSupportedProvider
+    openai = {
+      endpoint = "https://api.openai.com/v1",
+      model = "gpt-4o",
+      timeout = 30000, -- Timeout in milliseconds
       temperature = 0,
       max_tokens = 4096,
+    },
+    ---@type AvanteSupportedProvider
+    copilot = {
+      endpoint = "https://api.githubcopilot.com",
+      model = "gpt-4o-2024-08-06",
+      proxy = nil, -- [protocol://]host[:port] Use this proxy
+      allow_insecure = false, -- Allow insecure server connections
+      timeout = 30000, -- Timeout in milliseconds
+      temperature = 0,
+      max_tokens = 4096,
+    },
+    ---@type AvanteSupportedProvider
+    gemini = {
+      endpoint = "https://generativelanguage.googleapis.com/v1beta/models",
+      model = "gemini-1.5-flash-latest",
+      timeout = 30000, -- Timeout in milliseconds
+      temperature = 0,
+      max_tokens = 4096,
+    },
+    ---Specify the special dual_boost mode
+    ---1. enabled: Whether to enable dual_boost mode. Default to false.
+    ---2. first_provider: The first provider to generate response. Default to "openai".
+    ---3. second_provider: The second provider to generate response. Default to "claude".
+    ---4. prompt: The prompt to generate response based on the two reference outputs.
+    ---5. timeout: Timeout in milliseconds. Default to 60000.
+    ---How it works:
+    --- When dual_boost is enabled, avante will generate two responses from the first_provider and second_provider respectively. Then use the response from the first_provider as provider1_output and the response from the second_provider as provider2_output. Finally, avante will generate a response based on the prompt and the two reference outputs, with the default Provider as normal.
+    ---Note: This is an experimental feature and may not work as expected.
+    dual_boost = {
+      enabled = false,
+      first_provider = "openai",
+      second_provider = "claude",
+      prompt = "Based on the two reference outputs below, generate a response that incorporates elements from both but reflects your own judgment and unique perspective. Do not provide any explanation, just give the response directly. Reference Output 1: [{{provider1_output}}], Reference Output 2: [{{provider2_output}}]",
+      timeout = 60000, -- Timeout in milliseconds
+    },
+    behaviour = {
+      auto_suggestions = false, -- Experimental stage
+      auto_set_highlight_group = true,
+      auto_set_keymaps = true,
+      auto_apply_diff_after_generation = false,
+      support_paste_from_clipboard = false,
+      minimize_diff = true, -- Whether to remove unchanged lines when applying a code block
+      enable_token_counting = true, -- Whether to enable token counting. Default to true.
+      enable_cursor_planning_mode = false, -- Whether to enable Cursor Planning Mode. Default to false.
+    },
+    rag_service = {
+      enabled = false, -- Enables the rag service, requires OPENAI_API_KEY to be set
+      host_mount = os.getenv("HOME"), -- Host mount path for the rag service (docker will mount this path)
+      runner = "docker", -- The runner for the rag service, (can use docker, or nix)
+      provider = "openai", -- The provider to use for RAG service. eg: openai or ollama
+      llm_model = "", -- The LLM model to use for RAG service
+      embed_model = "", -- The embedding model to use for RAG service
+      endpoint = "https://api.openai.com/v1", -- The API endpoint for RAG service
     },
     file_selector = {
       --- @alias FileSelectorProvider "native" | "fzf" | "mini.pick" | "snacks" | "telescope" | string
