ff6015869d85d76d1672166386dc18783c302fbe
Your Name <you@example.com>
2025-05-08 19:55:15 +0100

    replace satellite.lua with scrollbar.lua due to performance

--- lua/plugins/satellite.lua
+++
@@ -1,8 +0,0 @@
-return {
-  "lewis6991/satellite.nvim",
-  lazy = true,
-  event = { "BufReadPost", "BufNewFile" },
-  opts = {
-    excluded_filetypes = {"AvanteInput", "Avante", "AvanteSelectedFiles"},
-  },
-}
---
+++ lua/plugins/scrollbar.lua
@@ -0,0 +1,20 @@
+return {
+  "petertriho/nvim-scrollbar",
+  lazy = true,
+  event = { "BufReadPost", "BufNewFile" },
+  opts = {
+    excluded_filetypes = {
+      "dropbar_menu",
+      "dropbar_menu_fzf",
+      "DressingInput",
+      "cmp_docs",
+      "cmp_menu",
+      "noice",
+      "prompt",
+      "TelescopePrompt",
+      "AvanteInput",
+      "Avante",
+      "AvanteSelectedFiles",
+    },
+  },
+}
